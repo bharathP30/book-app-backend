0d60330f7960e7f814ddcb390d938ab6b77d4186
bharathP30 <bharath145p@gmail.com>
2026-02-17 13:16:13 +0530

    Port error fix

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.json());
             app.use("/api/books", Authenticate, bookRouter);
             app.use("/api/auth", authRouter);
 
-        const PORT = 3000;
+        const PORT = process.env.PORT || 3000;
 
         app.listen(PORT, () => {
                 console.log("Express server is running on port ", PORT);
